43861316284806293ad1302c9a278d15ea7d864e
Alex Semin <asemin@gradle.com>
2024-11-05 19:02:03 +0100

    Add Develocity plugin

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,7 +1,22 @@
-rootProject.name = "trace-command-line-tool"
-
 pluginManagement {
     includeBuild("build-logic")
+}
+
+plugins {
+    id("com.gradle.develocity") version "3.18.1"
+}
+
+rootProject.name = "trace-command-line-tool"
+
+develocity {
+    buildScan {
+        termsOfUseUrl = "https://gradle.com/terms-of-service"
+        termsOfUseAgree = "yes"
+        // TODO: workaround for https://github.com/gradle/gradle/issues/22879.
+        val isCI = providers.environmentVariable("CI").isPresent
+        publishing.onlyIf { isCI }
+    }
+}
 }
 
 include("app")
