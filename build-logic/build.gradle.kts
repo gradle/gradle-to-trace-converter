923ca82bf67e7076bbc252657f502a75a2115df5
Alex Semin <asemin@gradle.com>
2024-11-05 21:32:46 +0100

    Upgrade to Kotlin 2.0.21

--- build-logic/build.gradle.kts
+++ build-logic/build.gradle.kts
@@ -7,7 +7,7 @@ repositories {
 }
 
 dependencies {
-    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.8.0") {
+    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:2.0.21") {
         because("Required to apply Kotlin JVM plugin")
     }
 }
